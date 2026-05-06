bc09b8bc764618c3e06aecea0bd2c54cb3cc652f
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-06 19:37:46 +0900

    Update keymap.c

--- keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
@@ -186,35 +186,35 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 // --- ここから追加 ---
 void rgb_matrix_indicators_user(void) {
     switch (get_highest_layer(layer_state)) {
-        default: // レイヤー0
-            // レイヤー0（デフォルト）は何もしない（設定したアニメーションが流れる）
+        case ONOFF: // レイヤー1
+            rgb_matrix_set_color_all(100, 149, 237); // Cornflowerblue
             break;
-        case 1: // レイヤー1
-            rgb_matrix_set_color_all(100,149,237); // Cornflowerblue
+        case OFFON: // レイヤー2
+            rgb_matrix_set_color_all(0, 255, 0); // 緑
             break;
-        case 2: // レイヤー2
-            rgb_matrix_set_color_all(0,255,0); // 緑
+        case ONON: // レイヤー3
+            rgb_matrix_set_color_all(0, 0, 255); // 青
             break;
-        case 3: // レイヤー3
-            rgb_matrix_set_color_all(0,0,255); // 青
+        case LOWER: // レイヤー4
+            rgb_matrix_set_color_all(0, 0, 255); // 青
             break;
-        case 4: // レイヤー4
-            rgb_matrix_set_color_all(0,0,255); // 青
+        case UPPER: // レイヤー5
+            rgb_matrix_set_color_all(0, 0, 255); // 青
             break;
-        case 5: // レイヤー5
-            rgb_matrix_set_color_all(0,0,255); // 青
+        case UTIL: // レイヤー6
+            rgb_matrix_set_color_all(0, 0, 255); // 青
             break;
-        case 6: // レイヤー6
-            rgb_matrix_set_color_all(0,0,255); // 青
+        case MOUSE: // レイヤー7
+            rgb_matrix_set_color_all(124, 252, 0); // Lawngreen
             break;
-        case 7: // レイヤー7 MOUSE
-            rgb_matrix_set_color_all(124,252,0); // Lawngreen
+        case BALL_SETTINGS: // レイヤー8
+            rgb_matrix_set_color_all(0, 255, 255); // aqua
             break;
-        case 8: // レイヤー8 BALL_SETTINGS
-            rgb_matrix_set_color_all(0,255,255); // aqua
+        case MISC: // レイヤー9
+            rgb_matrix_set_color_all(0, 0, 255); // Blue
             break;
-        case 9: // レイヤー9 MISC
-            rgb_matrix_set_color_all(0,0,255); // Blue
+        default: // BASE (レイヤー0) 
+            // 何もしない（設定したアニメーションが流れる）
             break;
     }
 }
