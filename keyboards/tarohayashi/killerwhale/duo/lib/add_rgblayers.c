067da35f715b7486bcb4d362d8a172f28f21528c
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-07 17:42:45 +0900

    Update add_rgblayers.c

--- keyboards/tarohayashi/killerwhale/duo/lib/add_rgblayers.c
+++ keyboards/tarohayashi/killerwhale/duo/lib/add_rgblayers.c
@@ -5,14 +5,14 @@
 #include "lib/common_killerwhale.h"
 
 // レイヤーごとの発光場所、発光色の設定
-const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 0, HSV_Lime} );
+const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 0, HHSV_AZURE} );
 const rgblight_segment_t PROGMEM onoff_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_Marigold} );
 const rgblight_segment_t PROGMEM offon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_MAGENTA} );
-const rgblight_segment_t PROGMEM onon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_WHITE} );
+const rgblight_segment_t PROGMEM onon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_GREEN} );
 const rgblight_segment_t PROGMEM lower_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_BLUE} );
 const rgblight_segment_t PROGMEM upper_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_CYAN} );
 const rgblight_segment_t PROGMEM util_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_CORAL} );
-const rgblight_segment_t PROGMEM mouse_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_Raphael} );
+const rgblight_segment_t PROGMEM mouse_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_PINK} );
 const rgblight_segment_t PROGMEM ball_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_GREEN} );
 const rgblight_segment_t PROGMEM light_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_PINK} );
 
