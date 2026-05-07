16106ebb136f52607082ccb0a41a551244e86fc2
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-07 15:04:24 +0900

    Refactor comments in keymap.c for clarity

--- keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
@@ -26,16 +26,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LNG2, KC_SPC,
         // 十字キーorジョイスティック                // ジョイスティックスイッチ
         KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,         L_CHMOD,
-        // 追加スイッチ                             // トグルスイッチ
+        // 追加スイッチ                       // トグルスイッチ
         MS_BTN2, MS_BTN1,                    MO(ONOFF),
+
         // 右手
         KC_6, LT(BALL_SETTINGS, KC_7), KC_8, KC_9, KC_0, KC_BSPC,
         KC_Y, KC_U, KC_I,    KC_O,             KC_P,    KC_ENT,
         KC_H, KC_J, KC_K,    LT(MOUSE, KC_L),  KC_SCLN, KC_RSFT,
         KC_N, KC_M, KC_COMM, KC_DOT,           KC_SLSH,
                              MOD_SCRL,
+        // 側面スイッチ
         KC_SPACE, KC_LNG1,
         KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,         R_CHMOD,
+        // 追加スイッチ                       // トグルスイッチ
         MS_BTN1, MS_BTN2,                    MO(OFFON)
     ),
     [ONOFF] = LAYOUT(
