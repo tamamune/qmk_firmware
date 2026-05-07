b7310fe1ff11ac5785f511e042959fe808968f98
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-07 17:51:38 +0900

    Update add_rgblayers.c

--- keyboards/tarohayashi/killerwhale/duo/lib/add_rgblayers.c
+++ keyboards/tarohayashi/killerwhale/duo/lib/add_rgblayers.c
@@ -5,7 +5,7 @@
 #include "lib/common_killerwhale.h"
 
 // レイヤーごとの発光場所、発光色の設定
-const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 0, HHSV_AZURE} );
+const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 0, HSV_AZURE} );
 const rgblight_segment_t PROGMEM onoff_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_CHARTREUSE} );
 const rgblight_segment_t PROGMEM offon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_MAGENTA} );
 const rgblight_segment_t PROGMEM onon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_GREEN} );
