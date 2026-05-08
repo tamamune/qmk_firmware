0afa0b6e7a0619dfca74d66cb1f33c8a5eb391f4
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-08 16:37:10 +0900

    Update keymap.c

--- keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_6, LT(BALL_SETTINGS, KC_7), KC_8, KC_9, KC_0, KC_BSPC,
         KC_Y, KC_U,    KC_I,   KC_O,     KC_P,  KC_ENT,
         KC_H, KC_J,    KC_K,   KC_L,  KC_SCLN, KC_MINUS,
-        LT(ONOFF,KC_N), LT(OFFON,KC_M), KC_COMM, KC_DOT,  KC_SLSH,
+        LT(ONOFF,KC_N), LT(OFFON,KC_M), KC_COMM, KC_DOT, KC_SLSH,
                                      MOD_SCRL,
         // 側面スイッチ
         KC_SPACE, KC_ENT,
