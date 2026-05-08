4119b0ed976fea8c0b0babc4212f0697088b181f
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-08 16:22:15 +0900

    Update keymap.c

--- keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
@@ -43,12 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD(TD_ESC_GRAVE), KC_1, KC_2, KC_3, KC_4, KC_5,
         KC_TAB,  KC_Q,         KC_W, KC_E, KC_R, KC_T,
         CMD_CTL, KC_A,         KC_S, KC_D, KC_F, KC_G,
-                 LSFT_T(KC_Z), KC_X,       KC_C, KC_V, LT(ONOFF,KC_B),
+                 LSFT_T(KC_Z), KC_X,       LT(OFFON,KC_C), KC_V, LT(ONOFF,KC_B),
                                       KC_LWIN,
         // 側面スイッチ
         KC_SPACE, KC_ENT,
         // 十字キーorジョイスティック                // ジョイスティックスイッチ
-        KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,         L_CHMOD,
+        KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT         L_CHMOD,
         // 追加スイッチ                       // トグルスイッチ
         MO(LOWER), MO(UPPER),                MO(ONOFF),
 
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_6, LT(BALL_SETTINGS, KC_7), KC_8, KC_9, KC_0, KC_BSPC,
         KC_Y, KC_U,    KC_I,   KC_O,     KC_P,  KC_ENT,
         KC_H, KC_J,    KC_K,   KC_L,  KC_SCLN, KC_MINUS,
-        LT(ONOFF,KC_N), KC_M, KC_COMM, KC_DOT,  KC_SLSH,
+        LT(ONOFF,KC_N), LT(OFFON,KC_M), KC_COMM, KC_DOT,  KC_SLSH,
                                      MOD_SCRL,
         // 側面スイッチ
         KC_SPACE, KC_ENT,
