3688d22442bc955995935f6f92d66dde16aac2fd
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-09 11:26:17 +0900

    Update keymap.c

--- keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/test/keymap.c
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(LOWER), MO(UPPER),                MO(ONOFF),
 
         // 右手
-                  KC_6,           KC_7,    KC_8,   KC_9,    KC_0,  KC_BSPC,
-                  KC_Y,           KC_U,    KC_I,   KC_O,    KC_P,   KC_ENT,
-                  KC_H,           KC_J,    KC_K,   KC_L, KC_SCLN, KC_MINUS,
-        LT(ONOFF,KC_N), LT(OFFON,KC_M), KC_COMM, KC_DOT, KC_SLSH,
-                                               MOD_SCRL,
+                  KC_6,           KC_7,    KC_8,   KC_9,    KC_0, KC_MINS,
+                  KC_Y,           KC_U,    KC_I,   KC_O,    KC_P,  KC_EQL,
+                  KC_H,           KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT,
+        LT(ONOFF,KC_N), LT(OFFON,KC_M), KC_COMM, KC_DOT, KC_RBRC,
+                                                KC_LBRC,
         // 側面スイッチ 下 上
         KC_ENT, KC_SPACE,
         KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,         R_CHMOD,
