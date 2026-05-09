79592571c68c8a530c3c127fbce3286e3a2a2d18
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-05-09 23:04:04 +0900

    Update add_rgblayers.c

--- keyboards/tarohayashi/killerwhale/duo/lib/add_rgblayers.c
+++ keyboards/tarohayashi/killerwhale/duo/lib/add_rgblayers.c
@@ -6,7 +6,7 @@
 
 // レイヤーごとの発光場所、発光色の設定
 const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 0, 255, 0, 255} );
-const rgblight_segment_t PROGMEM onoff_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_BLUE} );
+const rgblight_segment_t PROGMEM onoff_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, 0, 255, 255} );
 const rgblight_segment_t PROGMEM offon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_MAGENTA} );
 const rgblight_segment_t PROGMEM onon_layer[] = RGBLIGHT_LAYER_SEGMENTS( { 0, 66, HSV_GREEN} );
 const rgblight_segment_t PROGMEM lower_layer[] = RGBLIGHT_LAYER_SEGMENTS( {0, 66, HSV_BLUE} );
