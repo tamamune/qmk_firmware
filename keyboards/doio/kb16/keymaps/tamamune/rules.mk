080b0c06172033fd4d7ff71fe83369a3b0cc2119
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-08 02:13:35 +0900

    Update rules.mk

--- keyboards/doio/kb16/keymaps/tamamune/rules.mk
+++ keyboards/doio/kb16/keymaps/tamamune/rules.mk
@@ -1,4 +1,7 @@
-# Encoder enabled
-ENCODER_MAP_ENABLE = yes
+# WS2812の制御方式をPWM(DMA)に切り替え
+WS2812_DRIVER = pwm
+
+# 機能の有効化と競合防止
 VIA_ENABLE = yes
-LTO_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no
