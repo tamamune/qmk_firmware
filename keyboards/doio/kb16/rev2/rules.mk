d88523ced80540816c0d159896eb975e8642246a
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-08 16:53:16 +0900

    Update rules.mk

--- keyboards/doio/kb16/rev2/rules.mk
+++ keyboards/doio/kb16/rev2/rules.mk
@@ -1,5 +1,6 @@
-SRC += ./lib/layer_status/layer_status.c
-SRC += ./lib/logo.c
+# OLED用カスタムファイルの読み込みを無効化（行頭に # を追加）
+# SRC += ./lib/layer_status/layer_status.c
+# SRC += ./lib/logo.c
 
 # Configure for 128K flash
 MCU_LDSCRIPT = STM32F103xB
