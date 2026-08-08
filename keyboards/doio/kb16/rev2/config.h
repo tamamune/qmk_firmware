1b9bb8d39945a47de46423231209e7cc12bf58c2
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-08 11:48:47 +0900

    Add OLED and RGB update frequency settings
    Added definitions to reduce OLED and RGB update frequencies.

--- keyboards/doio/kb16/rev2/config.h
+++ keyboards/doio/kb16/rev2/config.h
@@ -2,3 +2,7 @@
 
 /* Custom OLED Font */
 #define OLED_FONT_H "./lib/glcdfont.c"
+
+/* ★OLEDとRGBの更新周波数を下げてCPU割り込み（チラつき）を防ぐ */
+#define OLED_UPDATE_INTERVAL 50   /* OLED更新頻度を抑える */
+#define RGB_MATRIX_FPS 20         /* RGBフレームレートを抑える */
