1ced5ed96a00ee3865ea590836ba3634c42e6d78
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-08 12:17:09 +0900

    Update config.h

--- keyboards/doio/kb16/rev2/config.h
+++ keyboards/doio/kb16/rev2/config.h
@@ -1,10 +1,8 @@
 #pragma once
 
-/* Custom OLED Font */
-#define OLED_FONT_H "./lib/glcdfont.c"
-
-/* ★親設定の定義を一度解除してから再定義する */
-#undef RGB_MATRIX_FPS
-#define RGB_MATRIX_FPS 20
-
-#define OLED_UPDATE_INTERVAL 50
+#ifdef OLED_ENABLE
+    /* Mapping I2C2 for OLED */
+    #define I2C1_SCL_PIN B10
+    #define I2C1_SDA_PIN B11
+    #define I2C_DRIVER I2CD2
+#endif
