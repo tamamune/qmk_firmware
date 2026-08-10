5d4a26ca91e7ee4dd255435cc35b9ec75e214a52
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-10 19:06:07 +0900

    Update kb16.c

--- keyboards/doio/kb16/kb16.c
+++ keyboards/doio/kb16/kb16.c
@@ -33,29 +33,22 @@
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
-bool rgb_matrix_indicators_kb(void) {
-    if (!rgb_matrix_indicators_user()) {
-        return false;
-    }
-    // ★ RGBがOFF（消灯）の時は強制的にすべてのLEDを消灯（黒）にする
-    if (!rgb_matrix_is_enabled()) {
-        rgb_matrix_set_color_all(0, 0, 0);
-        return false;
-    }
-    return true;
-}
-
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-    if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
-        return false;
-    }
-    // ★ RGBがOFF（消灯）の時は描画範囲の全LEDを黒に上書きする
+    // RGBがOFFの時は全LED範囲を黒(0,0,0)にして強制消灯
     if (!rgb_matrix_is_enabled()) {
         for (uint8_t i = led_min; i < led_max; i++) {
             rgb_matrix_set_color(i, 0, 0, 0);
         }
         return false;
     }
-    return true;
+    return rgb_matrix_indicators_advanced_user(led_min, led_max);
+}
+
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_is_enabled()) {
+        rgb_matrix_set_color_all(0, 0, 0);
+        return false;
+    }
+    return rgb_matrix_indicators_user();
 }
 #endif
