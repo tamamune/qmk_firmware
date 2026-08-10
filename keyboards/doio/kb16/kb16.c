744f8deba12cced9b7bad5a9e9f090e76dd45382
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-10 21:13:51 +0900

    Update kb16.c

--- keyboards/doio/kb16/kb16.c
+++ keyboards/doio/kb16/kb16.c
@@ -1,5 +1,4 @@
 #include "quantum.h"
-#include "rgb_matrix.h"
 
 // OLED animation
 #include "./lib/logo.h"
@@ -33,25 +32,24 @@
     }
 #endif
 
-void matrix_scan_kb(void) {
-#ifdef RGB_MATRIX_ENABLE
-    // RGBがOFFの時は毎スキャンで全LEDへ黒(0,0,0)をセットし消灯を確定させる
-    if (!rgb_matrix_is_enabled()) {
-        rgb_matrix_set_color_all(0, 0, 0);
-    }
-#endif
-    matrix_scan_user();
-}
-
 #ifdef RGB_MATRIX_ENABLE
 #include "rgb_matrix.h"
 
-// 基板側での白色上書き処理を一切行わず、VIAやRGB_TOGの制御に任せる
-bool rgb_matrix_indicators_kb(void) {
-    return true;
+bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+    if (!rgb_matrix_is_enabled()) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        }
+        return false;
+    }
+    return rgb_matrix_indicators_advanced_user(led_min, led_max);
 }
 
-bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
-    return true;
+bool rgb_matrix_indicators_kb(void) {
+    if (!rgb_matrix_is_enabled()) {
+        rgb_matrix_set_color_all(0, 0, 0);
+        return false;
+    }
+    return rgb_matrix_indicators_user();
 }
 #endif
