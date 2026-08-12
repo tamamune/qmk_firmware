38718181d21840ce71f64344d2e6b5d40e0ee0c3
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-12 10:26:55 +0900

    Update rules.mk

--- keyboards/doio/kb16/rev2/rules.mk
+++ keyboards/doio/kb16/rev2/rules.mk
@@ -2,4 +2,17 @@ SRC += ./lib/layer_status/layer_status.c
 SRC += ./lib/logo.c
 
 # Configure for 128K flash
-MCU_LDSCRIPT = STM32F103xB
+/*MCU_LDSCRIPT = STM32F103xB*/
+/*上はデフォルト*/
+
+/*下は書き換え部分*/
+# MCU name
+MCU = STM32F103
+
+# OLED enabled
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+
+# RGB Matrix enabled
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
