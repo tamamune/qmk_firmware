c1f5ab57b5694c85c26fc5e12917aa98fb624ae5
tamamune <moldman.the.bellringer.in.dublin@gmail.com>
2026-08-12 11:19:12 +0900

    Update rules.mk

--- keyboards/doio/kb16/rev2/rules.mk
+++ keyboards/doio/kb16/rev2/rules.mk
@@ -3,9 +3,11 @@ SRC += ./lib/logo.c
 
 # Configure for 128K flash
 # MCU_LDSCRIPT = STM32F103xB
+
 # 上はデフォルト
 
 # 下は書き換え部分
+
 # MCU name
 MCU = STM32F103
 
@@ -19,6 +21,20 @@ BOOTLOADER_TYPE = stm32duino
 DFU_ARGS = -d 1EAF:0003 -a 2 -R
 DFU_SUFFIX_ARGS = -v 1EAF -p 0003
 
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+USE_VIA = yes
+
 # OLED enabled
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
@@ -26,3 +42,10 @@ OLED_DRIVER = SSD1306
 # RGB Matrix enabled
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
+
+# Encoder enabled
+ENCODER_ENABLE = yes
+
+# Wear-levelling driver
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
